7e193d75158305dcbca062abbfec997e8aaaa8a8
bruno.chavez <darklaier@gmail.com>
2019-04-24 00:57:22 -0400

    Getters y Setters de admin

--- src/main/java/cl/usm/prevencionderiesgos/si/models/Admin.java
+++ src/main/java/cl/usm/prevencionderiesgos/si/models/Admin.java
@@ -6,6 +6,30 @@ import javax.persistence.Id;
 
 @Entity
 public class Admin {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Id
     @GeneratedValue
     private Integer id;
