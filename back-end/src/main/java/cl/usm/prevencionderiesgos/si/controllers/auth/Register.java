e968bef41470bbdab055b6bcb0f0b9c0a4230816
bruno-chavez <darklaier@gmail.com>
2019-06-14 19:50:13 -0400

    Encriptacion de contraseñas añadida.

--- back-end/src/main/java/cl/usm/prevencionderiesgos/si/controllers/auth/Register.java
+++ back-end/src/main/java/cl/usm/prevencionderiesgos/si/controllers/auth/Register.java
@@ -2,23 +2,31 @@ package cl.usm.prevencionderiesgos.si.controllers.auth;
 
 import cl.usm.prevencionderiesgos.si.models.Student;
 import cl.usm.prevencionderiesgos.si.repositories.StudentRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/register")
 public class Register {
 
     private final StudentRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    Register(StudentRepository repository) {
+    Register(StudentRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+
     @PostMapping
-    Student create(@RequestBody Student student) {
+    Student RegisterUser(@RequestBody Student student) {
+
+        student.setPassword(passwordEncoder.encode(student.getPassword()));
+
         return repository.save(student);
     }
 }
