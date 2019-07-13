aaf15e4793923fe32efe077ddab6aab7b1b78a92
bruno-chavez <darklaier@gmail.com>
2019-07-13 15:41:21 -0400

    Componente Home eliminado y vista inicial se cambio a Login.

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,25 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
-Vue.use(Router)
+// Auth
+import Login from './components/Auth/Login.vue'
+import Register from './components/Auth/Register.vue'
+
+
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
+  routes: [{
+      path: '/register',
+      name: 'register',
+      component: Register
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
+      path: '/',
+      name: 'login',
+      component: Login
     }
   ]
 })
