0ec47d58297555fa2589dd10d445c3f3bb1db5b6
bruno-chavez <darklaier@gmail.com>
2019-07-18 02:25:10 -0400

    Añadido modelos para respuestas de controladores

---
+++ back-end/src/main/java/cl/usm/prevencionderiesgos/si/DTOs/Message.java
@@ -0,0 +1,14 @@
+package cl.usm.prevencionderiesgos.si.DTOs;
+
+public class Message {
+
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
