2a8d9d71b99411a2fdc1825f9b0c500ad95baa85
bruno-chavez <darklaier@gmail.com>
2019-07-18 02:28:29 -0400

    Añadida tipo de query a AdminRepository

--- back-end/src/main/java/cl/usm/prevencionderiesgos/si/repositories/AdminRepository.java
+++ back-end/src/main/java/cl/usm/prevencionderiesgos/si/repositories/AdminRepository.java
@@ -1,10 +1,12 @@
 package cl.usm.prevencionderiesgos.si.repositories;
 
 import cl.usm.prevencionderiesgos.si.models.Admin;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdminRepository extends CrudRepository<Admin, Long> {
+    Admin findByEmail(String email);
 
 }
